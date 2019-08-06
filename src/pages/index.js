2b98ddca41022f92686cf94431612b244be8f6cc
Gustav Burchardt <gustavburchardt@gmail.com>
2019-08-06 14:02:12 +0200

    hooks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,28 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { graphql } from 'gatsby'
 
-export default ({ data }) => (
-  <div>
-    <h1>{data.site.siteMetadata.title}</h1>
-    <div>Hello kvalifik!</div>
-  </div>
-)
+export default ({ data }) => {
+  const [isChecked, setChecked] = useState(false)
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    document.title = search
+  }, [search])
+
+  const onToggle = () => {
+    setChecked(!isChecked)
+  }
+
+  return (
+    <div>
+      <h1>{data.site.siteMetadata.title}</h1>
+      {isChecked && <p>Hello kvalifik!</p>}
+      {!isChecked && <p>Goodbye kvalifik!</p>}
+      <input type="button" onClick={onToggle} value="Toggle" />
+      <input type="text" onChange={({ target: { value } }) => setSearch(value)} value={search} />
+    </div>
+  )
+}
 
 export const query = graphql`
   query {
