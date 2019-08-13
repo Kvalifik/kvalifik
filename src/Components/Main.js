5aa5bc899a617d0661cae604a60f323ba5d97133
Gustav Burchardt <gustavburchardt@gmail.com>
2019-08-13 09:58:23 +0200

    Remove typography

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,22 +1,30 @@
 import React from 'react'
-import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components'
 import theme from 'utils/theme'
 import PropTypes from 'prop-types'
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700,700i,900');
+
   body {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
 `
 
+const App = styled.div`
+  ${props => props.theme.mixins.typography.body()}
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`
+
 const Main = ({ children }) => (
   <>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
-      <div> {/* Add a surrounding div to make sure ThemeProvider only has a single child */}
+      <App> {/* Add a surrounding div to make sure ThemeProvider only has a single child */}
         {children}
-      </div>
+      </App>
     </ThemeProvider>
   </>
 )
