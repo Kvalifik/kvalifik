d3856f6925b86fc684f3d87b12bd0612f82242a0
Gustav Burchardt <gustavburchardt@gmail.com>
2019-08-28 10:26:36 +0200

    Move block proptypes

--- src/models/page.js
+++ src/models/page.js
@@ -1,8 +1,24 @@
 import PropTypes from 'prop-types'
-import { contentPropType } from 'blockTypes/content'
+import { headerPropType } from './blockTypes/header'
+import { actionblockPropType } from './blockTypes/actionblock'
+import { casegridPropType } from './blockTypes/casegrid'
+import { sloganblockPropType } from './blockTypes/sloganblock'
+import { overlayblockPropType } from './blockTypes/overlayBlock'
+import { caseinfoPropType } from './blockTypes/caseinfo'
+import { percentageblockPropType } from './blockTypes/percentageBlock'
+import { toolboxPropType } from './blockTypes/toolbox'
 
 export const pagePropType = PropTypes.shape({
-  pageSetup: contentPropType,
+  pageSetup: PropTypes.arrayOf(PropTypes.oneOfType([
+    headerPropType,
+    casegridPropType,
+    actionblockPropType,
+    sloganblockPropType,
+    overlayblockPropType,
+    caseinfoPropType,
+    percentageblockPropType,
+    toolboxPropType
+  ])),
   title: PropTypes.string,
   url: PropTypes.string
 })
