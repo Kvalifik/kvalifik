ae8301c97eb4b8c87cc90ff415dc482a2803f99a
Gustav Burchardt <gustavburchardt@gmail.com>
2019-08-28 10:40:18 +0200

    Rename block type folder

--- src/models/page.js
+++ src/models/page.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types'
-import { headerPropType } from './blockTypes/header'
-import { actionblockPropType } from './blockTypes/actionblock'
-import { casegridPropType } from './blockTypes/casegrid'
-import { sloganblockPropType } from './blockTypes/sloganblock'
-import { overlayblockPropType } from './blockTypes/overlayBlock'
-import { caseinfoPropType } from './blockTypes/caseinfo'
-import { percentageblockPropType } from './blockTypes/percentageBlock'
-import { toolboxPropType } from './blockTypes/toolbox'
+import { headerPropType } from './blocks/header'
+import { actionblockPropType } from './blocks/actionblock'
+import { casegridPropType } from './blocks/casegrid'
+import { sloganblockPropType } from './blocks/sloganblock'
+import { overlayblockPropType } from './blocks/overlayBlock'
+import { caseinfoPropType } from './blocks/caseinfo'
+import { percentageblockPropType } from './blocks/percentageBlock'
+import { toolboxPropType } from './blocks/toolbox'
 
 export const pagePropType = PropTypes.shape({
   pageSetup: PropTypes.arrayOf(PropTypes.oneOfType([
