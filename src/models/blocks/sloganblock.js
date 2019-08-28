283a773a2fca3af68f44aab82cf5f117aa7a6de0
Gustav Burchardt <gustavburchardt@gmail.com>
2019-08-28 15:38:23 +0200

    Remove block proptypes

--- src/models/blocks/sloganblock.js
+++
@@ -1,5 +0,0 @@
-import PropTypes from 'prop-types'
-
-export const sloganblockPropType = PropTypes.shape({
-  punchline: PropTypes.string
-})
--- src/models/page.js
+++ src/models/page.js
@@ -1,26 +1,7 @@
 import PropTypes from 'prop-types'
-import { headerPropType } from './blocks/header'
-import { actionblockPropType } from './blocks/actionblock'
-import { casegridPropType } from './blocks/casegrid'
-import { sloganblockPropType } from './blocks/sloganblock'
-import { overlayblockPropType } from './blocks/overlayBlock'
-import { caseinfoPropType } from './blocks/caseinfo'
-import { percentageblockPropType } from './blocks/percentageBlock'
-import { toolboxPropType } from './blocks/toolbox'
-import { quoteblockPropType } from './blocks/quoteblock'
 
 export const pagePropType = PropTypes.shape({
-  pageSetup: PropTypes.arrayOf(PropTypes.oneOfType([
-    headerPropType,
-    casegridPropType,
-    actionblockPropType,
-    sloganblockPropType,
-    overlayblockPropType,
-    caseinfoPropType,
-    percentageblockPropType,
-    toolboxPropType,
-    quoteblockPropType
-  ])),
+  pageSetup: PropTypes.array,
   title: PropTypes.string,
   url: PropTypes.string
 })
