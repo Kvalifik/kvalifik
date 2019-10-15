07917216627d20095fafedc8bb58af873f6fbd16
Nikolaj Schlüter <nikolaj@bitkompagniet.dk>
2019-10-15 15:02:22 +0200

    rm react-error-overlay

--- src/Components/AutoPlayVideo.js
+++ src/Components/AutoPlayVideo.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { errorMap } from '../../.cache/error-overlay-handler'
 
 const Root = styled.div`
   cursor: pointer;
